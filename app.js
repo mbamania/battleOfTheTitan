91414449ac0cf1ba7ce9641b463885a58e4cd8a6
mbamania <mbamania@hotmail.co.uk>
2024-02-21 11:43:14 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-// Connect to the Ethereum network (update with your provider)
-const web3 = new Web3('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+window.addEventListener('load', async () => {
+    // Check if MetaMask is installed
+    if (typeof window.ethereum !== 'undefined') {
+        console.log('MetaMask is installed!');
+    } else {
+        console.log('MetaMask is not installed!');
+        return;
+    }
+});
 
-// ABI (Application Binary Interface) of your smart contract
-const abi = [
-    // Define your smart contract's ABI here
-];
+async function connect() {
+    // Request account access
+    try {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const account = accounts[0];
+        document.getElementById('account').innerText = `Account: ${account}`;
 
-// Address of your smart contract
-const contractAddress = '0x6D3FfC14bb34aa9Dc88045a3Daf3D621Bd2dd3A6';
-
-// Create an instance of your smart contract
-const contract = new web3.eth.Contract(abi, contractAddress);
-
-// Function to get the balance from your smart contract
-async function getBalance() {
-    const account = '0xdEA8e90B07567CF462C5aCa25D053688E24777A2'; // Update with your Ethereum account address
-    const balance = await contract.methods.getBalance(account).call();
-    document.getElementById('balance').innerText = `Balance: ${balance}`;
+        // Get ETH balance
+        const balance = await web3.eth.getBalance(account);
+        document.getElementById('balance').innerText = `Balance: ${web3.utils.fromWei(balance)} ETH`;
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
